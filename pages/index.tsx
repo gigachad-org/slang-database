32449a6fd175a88556428909507d8c62b4c97348
John James <john.usa.james@gmail.com>
2022-11-27 19:31:14 +0000

    chore: add prettier and format all files

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import Head from "next/head";
+import Head from "next/head"
 
 export default function Home() {
   return (
@@ -17,5 +17,5 @@ export default function Home() {
         <p>footer</p>
       </footer>
     </>
-  );
+  )
 }
