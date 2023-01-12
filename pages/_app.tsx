b79c1e5bec10ee2d9703e92a1839cdd9c3909f36
John James <john.usa.james@gmail.com>
2023-01-12 07:28:02 +0000

    chore: configure mui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,27 @@
-import "../styles/globals.css"
+import Head from "next/head"
+import { ThemeProvider } from "@mui/material/styles"
+import CssBaseline from "@mui/material/CssBaseline"
 import type { AppProps } from "next/app"
 
+import "@fontsource/roboto/300.css"
+import "@fontsource/roboto/400.css"
+import "@fontsource/roboto/500.css"
+import "@fontsource/roboto/700.css"
+
+import theme from "../src/theme"
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  )
 }
