669ea9c898a1d2947769e7458bddfef483693b96
John James <john.usa.james@gmail.com>
2023-01-12 07:32:13 +0000

    chore: remove font imports

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,11 +3,6 @@ import { ThemeProvider } from "@mui/material/styles"
 import CssBaseline from "@mui/material/CssBaseline"
 import type { AppProps } from "next/app"
 
-import "@fontsource/roboto/300.css"
-import "@fontsource/roboto/400.css"
-import "@fontsource/roboto/500.css"
-import "@fontsource/roboto/700.css"
-
 import theme from "../src/theme"
 
 export default function App({ Component, pageProps }: AppProps) {
